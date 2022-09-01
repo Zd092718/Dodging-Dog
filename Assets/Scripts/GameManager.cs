e0a3724f4abc9a273ccfdec8320e712548be5cc9
Zack Dowd <zdowd2796@gmail.com>
2022-09-01 15:37:40 -0600

    Game over screen working

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
     [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private GameObject gameOverScreen;
+
     private bool isGameOver = false;
     private int score = 0;
 
@@ -18,7 +21,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        gameOverScreen.SetActive(false);
     }
 
     // Update is called once per frame
@@ -33,6 +36,8 @@ public class GameManager : MonoBehaviour
 
         ObstacleSpawner obstacleSpawner = GameObject.Find("ObstacleSpawner").GetComponent<ObstacleSpawner>();
         obstacleSpawner.StopSpawning();
+
+        gameOverScreen.SetActive(true);
     }
 
     public void IncrementScore()
@@ -43,5 +48,15 @@ public class GameManager : MonoBehaviour
 
             scoreText.text = score.ToString();
         }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
